0536d94fd89127a01b0ef7d2153b11e66e3338f5
LeafField <109466326+LeafField@users.noreply.github.com>
2023-06-28 23:14:45 +0900

    完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        normal: "16px",
+      },
+
       colors: {
         body: "#050505",
         "selected-text": "#a3a3ff",
         thema: "#5c318c",
+        secondary: "#9191a4",
+        badge: "#3f3f51",
+        inputBorder: "#565666",
+        input: "#2a2a33",
       },
     },
     fontFamily: {
